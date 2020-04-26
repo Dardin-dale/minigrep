a6d10bc89c6c2a10b03dc6d20e66adb159941bf8
Logan Crecraft <logandalec@gmail.com>
2020-04-26 13:26:43 -0700

    initial FS reading/args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //grab binary executables environment arguments
+    let args: Vec<String> = env::args().collect();
+    //only need these two arguments uses references
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    //read specified file, specify error if file cant be read
+    let contents = fs::read_to_string(filename)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
